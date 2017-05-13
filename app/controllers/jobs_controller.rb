0e8ea429e3d9ca2a7d5d9b68f5dfb9bdf8ef614f
Stephen Ashley <stephen.ashley.jr@gmail.com>
2017-05-13 13:50:37 -0400

    Initial Commit

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,22 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+
+  end
+private
+  def job_params
+    params.require(:job).permit(:title,:description)
+  end
+
+end
